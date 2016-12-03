18d2dac0a85f31d6b27112c6d17d5f3213488854
Amit Gupta <amit847.swastic@gmail.com>
2016-12-03 14:34:45 -0500

    Initial checkins

---
+++ ContactWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,65 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ContactWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/lib/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/js/lib/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/js/lib/jquery.dataTables.min.js",
+                        "~/js/lib/dataTables.bootstrap.min.js",
+                        "~/js/lib/dataTables.colReorder.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.min.css",
+                      "~/css/jquery.dataTables.min.css",
+                      "~/css/jquery.dataTables_themeroller.css",
+                      "~/css/dataTables.bootstrap.min.css",
+                      "~/css/colReorder.bootstrap.min.css",
+                      "~/css/site.css"));
+        }
+    }
+}
+
+
+
+//using System.Web;
+//using System.Web.Optimization;
+
+//namespace ContactWeb
+//{
+//    public class BundleConfig
+//    {
+//        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+//        public static void RegisterBundles(BundleCollection bundles)
+//        {
+//            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+//                        "~/Scripts/jquery-{version}.js"));
+
+//            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+//                        "~/Scripts/jquery.validate*"));
+
+//            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+//            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+//            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+//                        "~/Scripts/modernizr-*"));
+
+//            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+//                      "~/Scripts/bootstrap.js",
+//                      "~/Scripts/respond.js"));
+
+//            bundles.Add(new StyleBundle("~/Content/css").Include(
+//                      "~/Content/bootstrap.css",
+//                      "~/Content/site.css"));
+//        }
+//    }
+//}
